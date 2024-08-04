aa1b038872719c0da5f98130a119c730e152016a
phk <qhk1198@gmail.com>
2024-08-04 13:09:11 +0900

    chore: 오타 수정

--- src/main/java/com/example/fashionforecastbackend/global/BaseTimeEntity.java
+++ src/main/java/com/example/fashionforecastbackend/global/BaseTimeEntity.java
@@ -15,7 +15,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseTimeEntity {
 
     @CreatedDate
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     private LocalDateTime modifiedAt;
