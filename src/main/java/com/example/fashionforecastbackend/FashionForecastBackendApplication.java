6e2cdd4853359585a5b81006a45b145410a57263
phk <qhk1198@gmail.com>
2024-08-04 12:55:06 +0900

    chore: 백엔드 이니셜 세팅

---
+++ src/main/java/com/example/fashionforecastbackend/FashionForecastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fashionforecastbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionForecastBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionForecastBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fashionforecastbackend/FashionForecastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fashionforecastbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionForecastBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
