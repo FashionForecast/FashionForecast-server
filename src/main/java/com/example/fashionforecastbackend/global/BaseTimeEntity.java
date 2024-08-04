450cc9ad1a6a907bca7f316e5a45283e8b65279d
phk <qhk1198@gmail.com>
2024-08-04 13:05:27 +0900

    feat: BaseTimeEntity 구현

---
+++ src/main/java/com/example/fashionforecastbackend/global/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.example.fashionforecastbackend.global;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
