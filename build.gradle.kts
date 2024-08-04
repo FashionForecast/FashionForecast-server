4fd175cd4bcbddbfeadba7ecc3910fff25685d5e
phk <qhk1198@gmail.com>
2024-08-04 20:15:10 +0900

    chore: restDocs 라이브러리 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.8"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
 group = "com.example"
@@ -23,9 +24,12 @@ repositories {
     mavenCentral()
 }
 
+val snippetsDir = file("build/generated-snippets")
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
@@ -36,4 +40,38 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// Ascii Doc Create Tasks
+tasks {
+    // Test 결과를 snippet Directory에 출력
+    test {
+        outputs.dir(snippetsDir)
+    }
+
+    asciidoctor {
+        // test 가 성공해야만, 아래 Task 실행
+        dependsOn(test)
+
+        // 기존에 존재하는 Docs 삭제(문서 최신화를 위해)
+        doFirst {
+            delete(file("src/main/resources/static/docs"))
+        }
+
+        // snippet Directory 설정
+        inputs.dir(snippetsDir)
+
+        // Ascii Doc 파일 생성
+        doLast {
+            copy {
+                from("build/docs/asciidoc")
+                into("src/main/resources/static/docs")
+            }
+        }
+    }
+
+    build {
+        // Ascii Doc 파일 생성이 성공해야만, Build 진행
+        dependsOn(asciidoctor)
+    }
 }
