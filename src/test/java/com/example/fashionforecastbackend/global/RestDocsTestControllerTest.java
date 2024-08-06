fba32baf09b03a7599ce544bafab564508647e46
phk <qhk1198@gmail.com>
2024-08-06 17:26:09 +0900

    refactor: restDocsTest 메서드 이름 수정

--- src/test/java/com/example/fashionforecastbackend/global/RestDocsTestControllerTest.java
+++ src/test/java/com/example/fashionforecastbackend/global/RestDocsTestControllerTest.java
@@ -15,18 +15,21 @@ class RestDocsTestControllerTest extends RestDocsTest{
 
     @DisplayName("200 상태, Hello를 응답으로 반환 후 restDocs를 생성한다")
     @Test
-    void exampleTest() throws Exception {
+    void restDocs_스니펫_생성_확인() throws Exception {
+        //given
         this.mockMvc.perform(MockMvcRequestBuilders.get("/api/test"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello"))
                 .andDo(restDocs.document());
 
+        // when
         // 스니펫 생성 확인
         String snippetsDir = "build/generated-snippets/rest-docs-test-controller-test/example-test/";
 
         File requestSnippet = new File(snippetsDir + "http-request.adoc");
         File responseSnippet = new File(snippetsDir + "http-response.adoc");
 
+        //then
         assertThat(requestSnippet).exists();
         assertThat(responseSnippet).exists();
     }
