3f35ed4612127453080fc2d70aa9232941191838
phk <qhk1198@gmail.com>
2024-08-06 21:25:37 +0900

    refactor: 테스트코드 수정

--- src/test/java/com/example/fashionforecastbackend/global/RestDocsTestControllerTest.java
+++ src/test/java/com/example/fashionforecastbackend/global/RestDocsTestControllerTest.java
@@ -16,13 +16,12 @@ class RestDocsTestControllerTest extends RestDocsTest{
     @DisplayName("200 상태, Hello를 응답으로 반환 후 restDocs를 생성한다")
     @Test
     void restDocs_스니펫_생성_확인() throws Exception {
-        //given
+
+        // when & then
         this.mockMvc.perform(MockMvcRequestBuilders.get("/api/test"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello"))
                 .andDo(restDocs.document());
-
-        // when
         // 스니펫 생성 확인
         String snippetsDir = "build/generated-snippets/rest-docs-test-controller-test/example-test/";
 
