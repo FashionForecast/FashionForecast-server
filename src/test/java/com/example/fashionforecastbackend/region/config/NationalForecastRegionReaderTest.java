2cf47fd5167c7f3d0502893d32410a244ecbaa51
phk <qhk1198@gmail.com>
2024-08-13 16:30:51 +0900

    refactor: BDD 형식으로 수정

--- src/test/java/com/example/fashionforecastbackend/region/config/NationalForecastRegionReaderTest.java
+++ src/test/java/com/example/fashionforecastbackend/region/config/NationalForecastRegionReaderTest.java
@@ -1,14 +1,10 @@
 package com.example.fashionforecastbackend.region.config;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
-import com.example.fashionforecastbackend.region.domain.Region;
-import com.opencsv.CSVReader;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Set;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,81 +12,60 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.example.fashionforecastbackend.region.domain.Region;
+import com.opencsv.CSVReader;
+
 @ExtendWith(MockitoExtension.class)
 class NationalForecastRegionReaderTest {
 
-    @InjectMocks
-    private NationalForecastRegionReader nationalForecastRegionReader;
+	@InjectMocks
+	private NationalForecastRegionReader nationalForecastRegionReader;
 
-    @Mock
-    private CSVReader csvReader;
+	@Mock
+	private CSVReader csvReader;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        when(csvReader.readNext()).thenReturn(
-                new String[]{"code", "city", "district", "neighborhood", "nx", "ny"},
-                new String[]{"001", "서울특별시", "강남구", "삼성동", "60", "127"},
-                new String[]{"002", "부산광역시", "해운대구", "정동", "98", "76"},
-                null
-        );
-    }
+	@DisplayName("csv 파일을 정상적으로 읽어온다")
+	@Test
+	void read_정상_케이스() throws Exception {
+		//given
+		given(csvReader.readNext()).willReturn(new String[] {"code", "city", "district", "neighborhood", "nx", "ny"},
+			new String[] {"001", "서울특별시", "강남구", "삼성동", "60", "127"},
+			new String[] {"002", "부산광역시", "해운대구", "정동", "98", "76"},
+			null);
+		//when
+		Set<Region> regions = nationalForecastRegionReader.read();
 
-    @DisplayName("csv 파일을 정상적으로 읽어온다")
-    @Test
-    public void read_정상_케이스() {
-        //given
-        List<Region> regions = nationalForecastRegionReader.read();
+		//then
+		assertThat(regions).hasSize(2);
+	}
 
+	@DisplayName("잘못된 형식의 라인은 무시되고 읽어오지 않는다")
+	@Test
+	void read_잘못된_형식의_라인_무시() throws Exception {
 
-        //when & then
-        assertThat(regions).hasSize(2);
-        assertThat(regions.get(0).getAddress().getCode()).isEqualTo("001");
-        assertThat(regions.get(0).getAddress().getCity()).isEqualTo("서울특별시");
-        assertThat(regions.get(0).getAddress().getDistrict()).isEqualTo("강남구");
-        assertThat(regions.get(0).getAddress().getNeighborhood()).isEqualTo("삼성동");
-        assertThat(regions.get(0).getNx()).isEqualTo("60");
-        assertThat(regions.get(0).getNy()).isEqualTo("127");
-    }
+		//given
+		given(csvReader.readNext()).willReturn(
+			new String[] {"code", "city", "district", "neighborhood", "nx", "ny"},
+			new String[] {"001", "서울특별시", "강남구", "삼성동", "60", "127"},
+			new String[] {"002", "부산광역시"},
+			null
+		);
+		//when
+		Set<Region> regions = nationalForecastRegionReader.read();
 
-    @DisplayName("잘못된 형식의 라인은 무시되고 읽어오지 않는다")
-    @Test
-    void read_잘못된_형식의_라인_무시() throws Exception {
+		//then
+		assertThat(regions).hasSize(1);
+	}
 
-        //when
-        when(csvReader.readNext()).thenReturn(
-                new String[]{"code", "city", "district", "neighborhood", "nx", "ny"},
-                new String[]{"001", "서울특별시", "강남구", "삼성동", "60", "127"},
-                new String[]{"002", "부산광역시"},
-                null
-        );
+	@DisplayName("csv파일을 읽어오는 중 예외가 발생한다.")
+	@Test
+	void read_예외_발생() throws Exception {
+		//given
+		given(csvReader.readNext()).willThrow(new RuntimeException());
 
-        List<Region> regions = nationalForecastRegionReader.read();
-
-        //then
-        assertThat(regions).hasSize(1);
-        assertThat(regions.get(0).getAddress().getCode()).isEqualTo("001");
-        assertThat(regions.get(0).getAddress().getCity()).isEqualTo("서울특별시");
-        assertThat(regions.get(0).getAddress().getDistrict()).isEqualTo("강남구");
-        assertThat(regions.get(0).getAddress().getNeighborhood()).isEqualTo("삼성동");
-        assertThat(regions.get(0).getNx()).isEqualTo("60");
-        assertThat(regions.get(0).getNy()).isEqualTo("127");
-
-        assertThatThrownBy(() -> regions.get(1))
-                .isInstanceOf(IndexOutOfBoundsException.class);
-    }
-
-    @DisplayName("csv파일을 읽어오는 중 예외가 발생한다.")
-    @Test
-    public void read_예외_발생() throws Exception {
-
-        //when
-        when(csvReader.readNext()).thenThrow(new RuntimeException("파일 읽기 실패"));
-
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            nationalForecastRegionReader.read();
-        });
-
-        //then
-        assertThat(exception.getMessage()).isEqualTo("national_forecast_regions.csv을 읽어오는데 실패하였습니다.");
-    }
+		//when //then
+		assertThatThrownBy(() -> nationalForecastRegionReader.read())
+			.isInstanceOf(RuntimeException.class)
+			.hasMessage("national_forecast_regions.csv을 읽어오는데 실패하였습니다.");
+	}
 }
