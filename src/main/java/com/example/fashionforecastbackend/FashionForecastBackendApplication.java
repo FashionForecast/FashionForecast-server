129cb26c993977b7d9f82ece99332167047b254d
phk <qhk1198@gmail.com>
2024-08-13 15:31:18 +0900

    refactor: @EnableRetry, @EnableJpaAuditing 추가

--- src/main/java/com/example/fashionforecastbackend/FashionForecastBackendApplication.java
+++ src/main/java/com/example/fashionforecastbackend/FashionForecastBackendApplication.java
@@ -2,12 +2,16 @@ package com.example.fashionforecastbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableRetry
 public class FashionForecastBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FashionForecastBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FashionForecastBackendApplication.class, args);
+	}
 
 }
