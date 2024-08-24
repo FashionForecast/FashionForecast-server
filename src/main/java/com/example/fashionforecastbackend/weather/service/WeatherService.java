c26a599bdb073079b217f7b925751b1f57d7569e
phk <qhk1198@gmail.com>
2024-08-24 21:57:32 +0900

    feat: 날씨 조회 로직 구현 및 수정
    - getWeather() 내부 코드 메서드로 분리
    - getWeather() 외출 시간대의 날씨만 반환 하도록 수정
    - 계절에 따라 최고 또는 최저 기온 계산
    - 최대 강수확률 계산 추가
    - 최대 강수량 계산 추가

--- src/main/java/com/example/fashionforecastbackend/weather/service/WeatherService.java
+++ src/main/java/com/example/fashionforecastbackend/weather/service/WeatherService.java
@@ -1,10 +1,8 @@
 package com.example.fashionforecastbackend.weather.service;
 
-import java.util.List;
-
-import com.example.fashionforecastbackend.weather.dto.WeatherRequestDto;
-import com.example.fashionforecastbackend.weather.dto.WeatherResponseDto;
+import com.example.fashionforecastbackend.weather.dto.request.WeatherRequest;
+import com.example.fashionforecastbackend.weather.dto.response.WeatherResponse;
 
 public interface WeatherService {
-	List<WeatherResponseDto> getWeather(WeatherRequestDto dto);
+	WeatherResponse getWeather(WeatherRequest dto);
 }
