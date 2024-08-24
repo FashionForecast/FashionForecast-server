b902159814d813b45a1a351f902f5a61dc286170
phk <qhk1198@gmail.com>
2024-08-24 21:48:58 +0900

    add: 외출 시작 시간, 외출 끝 시간 추가

--- src/main/java/com/example/fashionforecastbackend/weather/dto/request/WeatherRequest.java
+++ src/main/java/com/example/fashionforecastbackend/weather/dto/request/WeatherRequest.java
@@ -1,4 +1,4 @@
-package com.example.fashionforecastbackend.weather.dto;
+package com.example.fashionforecastbackend.weather.dto.request;
 
 import java.time.LocalDateTime;
 
@@ -8,11 +8,19 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-public record WeatherRequestDto(
+public record WeatherRequest(
 
-	@NotNull(message = "시간을 입력해주세요.")
+	@NotNull(message = "현재 시간을 입력해주세요.")
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-	LocalDateTime now,
+	LocalDateTime nowDateTime,
+
+	@NotNull(message = "외출 시작 시간을 입력해주세요.")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	LocalDateTime startDateTime,
+
+	@NotNull(message = "외출 끝 시간을 입력해주세요.")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	LocalDateTime endDateTime,
 
 	@Min(value = 1, message = "위도는 1 이상이어야 합니다.")
 	@Max(value = 999, message = "위도는 999 이하여야 합니다.")
