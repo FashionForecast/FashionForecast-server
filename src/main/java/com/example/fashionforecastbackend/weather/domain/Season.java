bbf38c0a8238a4f07eb9c944b47ac8036410af22
phk <qhk1198@gmail.com>
2024-08-24 21:39:34 +0900

    feat: 계절 구현

---
+++ src/main/java/com/example/fashionforecastbackend/weather/domain/Season.java
@@ -0,0 +1,27 @@
+package com.example.fashionforecastbackend.weather.domain;
+
+import java.util.Arrays;
+
+import lombok.Getter;
+
+@Getter
+public enum Season {
+
+	SUMMER(5, 9),
+	WINTER(10, 4);
+
+	private final int startMonth;
+	private final int endMonth;
+
+	Season(int startMonth, int endMonth) {
+		this.startMonth = startMonth;
+		this.endMonth = endMonth;
+	}
+
+	public static Season fromMonth(int month) {
+		return Arrays.stream(Season.values())
+			.filter(season -> season.startMonth <= month && season.endMonth >= month)
+			.findAny()
+			.orElse(null);
+	}
+}
