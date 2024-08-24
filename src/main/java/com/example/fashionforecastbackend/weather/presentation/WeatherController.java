58bad366de8296679f8ab902df44db0612fbd1c5
phk <qhk1198@gmail.com>
2024-08-24 22:00:27 +0900

    refactor: 반환값 WeatherResponse로 수정

--- src/main/java/com/example/fashionforecastbackend/weather/presentation/WeatherController.java
+++ src/main/java/com/example/fashionforecastbackend/weather/presentation/WeatherController.java
@@ -1,15 +1,13 @@
 package com.example.fashionforecastbackend.weather.presentation;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.fashionforecastbackend.global.response.ApiResponse;
-import com.example.fashionforecastbackend.weather.dto.WeatherRequestDto;
-import com.example.fashionforecastbackend.weather.dto.WeatherResponseDto;
+import com.example.fashionforecastbackend.weather.dto.request.WeatherRequest;
+import com.example.fashionforecastbackend.weather.dto.response.WeatherResponse;
 import com.example.fashionforecastbackend.weather.service.WeatherService;
 
 import jakarta.validation.Valid;
@@ -23,8 +21,8 @@ public class WeatherController {
 	private final WeatherService weatherService;
 
 	@GetMapping("/forecast")
-	public ApiResponse<List<WeatherResponseDto>> getWeather(
-		@ModelAttribute @Valid WeatherRequestDto dto) {
+	public ApiResponse<WeatherResponse> getWeather(
+		@ModelAttribute @Valid WeatherRequest dto) {
 		return ApiResponse.ok(weatherService.getWeather(dto));
 	}
 
