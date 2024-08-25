66f1c04093b2bced056399f6eae8c4c2ee32adb8
JShistory <behappy0916@naver.com>
2024-08-25 22:00:20 +0900

    feat: SecurityConfig에 Oauth2설정 추가

--- src/main/java/com/example/fashionforecastbackend/global/config/SecurityConfig.java
+++ src/main/java/com/example/fashionforecastbackend/global/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.fashionforecastbackend.global.config;
 
+import com.example.fashionforecastbackend.oauth2.service.CustomOAuth2UserService;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,14 +18,22 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+	private final CustomOAuth2UserService customOAuth2UserService;
+
+	public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+		this.customOAuth2UserService = customOAuth2UserService;
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http
 			.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
-			.httpBasic(AbstractHttpConfigurer::disable);
-
+			.httpBasic(AbstractHttpConfigurer::disable)
+					.oauth2Login((oauth2) -> oauth2
+							.userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+									.userService(customOAuth2UserService))));
 		http
 			.authorizeHttpRequests(auth -> auth
 				.anyRequest().permitAll());
