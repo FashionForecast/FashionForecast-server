941fffba08a51fbd99a71588025d86bd64bbcfac
JShistory <behappy0916@naver.com>
2024-08-25 22:00:00 +0900

    feat: Member findByUsername 기능 추가

--- src/main/java/com/example/fashionforecastbackend/member/domain/repository/MemberRepository.java
+++ src/main/java/com/example/fashionforecastbackend/member/domain/repository/MemberRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.fashionforecastbackend.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-
+    Member findByUsername(String username);
 }
