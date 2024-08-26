4934cc1cf2ee79fcfa7bb7d0d77880847b41cb01
JShistory <behappy0916@naver.com>
2024-08-26 15:33:26 +0900

    refactor: CustomOAuth2UserServiceImpl 메소드 분리

--- src/main/java/com/example/fashionforecastbackend/oauth2/service/Impl/CustomOAuth2UserServiceImpl.java
+++ src/main/java/com/example/fashionforecastbackend/oauth2/service/Impl/CustomOAuth2UserServiceImpl.java
@@ -3,84 +3,105 @@ package com.example.fashionforecastbackend.oauth2.service.Impl;
 import com.example.fashionforecastbackend.member.domain.Member;
 import com.example.fashionforecastbackend.member.domain.MemberJoinType;
 import com.example.fashionforecastbackend.member.domain.MemberRole;
-import com.example.fashionforecastbackend.member.domain.repository.MemberRepository;
+import com.example.fashionforecastbackend.member.service.Impl.MemberServiceImpl;
 import com.example.fashionforecastbackend.oauth2.dto.CustomOAuth2User;
 import com.example.fashionforecastbackend.oauth2.dto.KakaoResponse;
 import com.example.fashionforecastbackend.oauth2.dto.OAuth2Response;
 import com.example.fashionforecastbackend.oauth2.dto.UserDTO;
-import java.util.Objects;
-
 import com.example.fashionforecastbackend.oauth2.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
+
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserServiceImpl extends DefaultOAuth2UserService implements CustomOAuth2UserService {
 
-    private final MemberRepository memberRepository;
+    private final MemberServiceImpl memberService;
+    private static MemberJoinType joinType;
 
-    public CustomOAuth2UserServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-    // todo Age, Gender 추가 시 수정 필요 !!
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2Response oAuth2Response = getOAuth2Response(userRequest);
+        String username = createUsername(oAuth2Response);
+
+        Optional<Member> optionalMember = memberService.findByUsername(username);
+
+        if (optionalMember.isEmpty()) {
+            log.info("새로운 회원입니다. 회원 정보를 저장합니다. : {}", oAuth2Response);
+            return registerNewUser(oAuth2Response, username);
+        } else {
+            log.info("이미 존재하는 회원입니다. 회원 정보를 업데이트합니다. : {}", oAuth2Response);
+            return updateExistingUser(optionalMember.get(), oAuth2Response);
+        }
+    }
+
+    // OAuth2Response 객체 생성 메서드
+    private OAuth2Response getOAuth2Response(OAuth2UserRequest userRequest) {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        OAuth2Response oAuth2Response = null;
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        MemberJoinType joinType = null;
-        //추가로 Oauth2가 확장될 수 있음
+
         if (Objects.equals(registrationId, "kakao")) {
-            oAuth2Response = new KakaoResponse(oAuth2User.getAttributes());
             joinType = MemberJoinType.KAKAO;
+            return new KakaoResponse(oAuth2User.getAttributes());
         }
 
-        //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
-        String username = oAuth2Response.getProvider() + "_" + oAuth2Response.getProviderId();
-        Member existData = memberRepository.findByUsername(username);
+        throw new IllegalArgumentException("Unsupported OAuth2 provider: " + registrationId);
+    }
 
-        if (existData == null) {
-            log.info("새로운 회원입니다. 회원 정보를 저장합니다. : {}",oAuth2Response);
-            existData = Member.builder()
-                    .username(username)
-                    .email(oAuth2Response.getEmail() != null ? oAuth2Response.getEmail() : null)
-                    .nickname(oAuth2Response.getName() != null ? oAuth2Response.getName() : null)
-                    .joinType(joinType)
-                    .role(MemberRole.MEMBER)
-                    .build();
+    // Username 생성 메서드
+    private String createUsername(OAuth2Response oAuth2Response) {
+        return oAuth2Response.getProvider() + "_" + oAuth2Response.getProviderId();
+    }
 
-            memberRepository.save(existData);
+    // 새로운 회원 등록 메서드
+    private OAuth2User registerNewUser(OAuth2Response oAuth2Response, String username) {
+        Member newMember = createNewMember(oAuth2Response, username);
+        memberService.save(newMember);
 
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUsername(username);
-            userDTO.setName(oAuth2Response.getName());
-            userDTO.setEmail(oAuth2Response.getEmail());
-            userDTO.setAge(oAuth2Response.getAge());
-            userDTO.setGender(oAuth2Response.getGender());
-            userDTO.setRole("ROLE_USER");
+        UserDTO userDTO = createUserDTO(newMember, oAuth2Response);
+        return new CustomOAuth2User(userDTO);
+    }
 
-            return new CustomOAuth2User(userDTO);
-        }
-        else {
-            log.info("이미 존재하는 회원입니다. 회원 정보를 업데이트합니다. : {}",oAuth2Response);
-            existData.updateNickname(oAuth2Response.getName());
-            existData.updateEmail(oAuth2Response.getEmail());
-            memberRepository.save(existData);
+    // 기존 회원 업데이트 메서드
+    private OAuth2User updateExistingUser(Member existingMember, OAuth2Response oAuth2Response) {
+        existingMember.updateNickname(oAuth2Response.getName());
+        existingMember.updateEmail(oAuth2Response.getEmail());
 
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUsername(existData.getUsername());
-            userDTO.setName(oAuth2Response.getName());
-            userDTO.setEmail(existData.getEmail());
-//            userDTO.setAge(existData.getAge());
-//            userDTO.setGender(existData.getGender());
-            userDTO.setRole("ROLE_USER");
-            return new CustomOAuth2User(userDTO);
-        }
+        memberService.save(existingMember);
 
+        UserDTO userDTO = createUserDTO(existingMember, oAuth2Response);
+        return new CustomOAuth2User(userDTO);
+    }
 
+    // 새로운 Member 객체 생성 메서드
+    private Member createNewMember(OAuth2Response oAuth2Response, String username) {
+        return Member.builder()
+                .username(username)
+                .email(oAuth2Response.getEmail())
+                .nickname(oAuth2Response.getName())
+                .joinType(joinType)
+                .role(MemberRole.MEMBER)
+                .build();
+    }
+
+    // UserDTO 생성 메서드
+    private UserDTO createUserDTO(Member member, OAuth2Response oAuth2Response) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(member.getUsername());
+        userDTO.setName(member.getNickname());
+        userDTO.setEmail(member.getEmail());
+        userDTO.setAge(oAuth2Response.getAge());
+        userDTO.setGender(oAuth2Response.getGender());
+        userDTO.setRole("ROLE_USER");
+        return userDTO;
     }
 }
