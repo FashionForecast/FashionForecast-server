f9b7a107a303c6ec67e911d6a39a74cfefa947c2
cherryiJuice <chldlwn711@gmail.com>
2024-08-27 21:17:11 +0900

    style: 코드 줄바꿈

--- src/main/java/com/example/fashionforecastbackend/outfit/domain/OutfitType.java
+++ src/main/java/com/example/fashionforecastbackend/outfit/domain/OutfitType.java
@@ -1,10 +1,10 @@
 package com.example.fashionforecastbackend.outfit.domain;
 
 public enum OutfitType {
-    TOP,
-    BOTTOM,
-    ETC,
-    LAYERED,
-    BASIC_UMBRELLA,
-    FOLDING_UMBRELLA
+	TOP,
+	BOTTOM,
+	ETC,
+	LAYERED,
+	BASIC_UMBRELLA,
+	FOLDING_UMBRELLA
 }
