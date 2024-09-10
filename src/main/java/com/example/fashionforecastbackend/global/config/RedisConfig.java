b56b1813f9cf6edc95860d2c0837d38c2b5271eb
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-09-10 17:50:12 +0900

    feat: RefreshToken 구현

--- src/main/java/com/example/fashionforecastbackend/global/config/RedisConfig.java
+++ src/main/java/com/example/fashionforecastbackend/global/config/RedisConfig.java
@@ -1,15 +1,19 @@
 package com.example.fashionforecastbackend.global.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import com.example.fashionforecastbackend.global.login.domain.RefreshToken;
 import com.example.fashionforecastbackend.search.domain.Search;
 
 import lombok.RequiredArgsConstructor;
@@ -17,8 +21,8 @@ import lombok.RequiredArgsConstructor;
 @EnableCaching
 @Configuration
 @RequiredArgsConstructor
+@EnableRedisRepositories(basePackages = "com.example.fashionforecastbackend.global.login.domain.repository", redisTemplateRef = "refreshTokenRedisTemplate")
 public class RedisConfig {
-
 
 	private final RedisProperties properties;
 
@@ -26,20 +30,26 @@ public class RedisConfig {
 	최근 검색어(Search)는 0번 데이터베이스 사용
 	 */
 	@Bean
-	public RedisConnectionFactory redisConnectionFactoryForSearch() {
-		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-		config.setHostName(properties.getHost());
-		config.setPort(properties.getPort());
-		config.setPassword(properties.getPassword());
-		config.setDatabase(0); // SELECT 0번 데이터베이스
-		return new LettuceConnectionFactory(config);
+	@Primary
+	LettuceConnectionFactory connectionFactory1() {
+		return createRedisConnectionFactory(0);
+	}
+
+	/*
+	리프레시 토큰(Refresh token)은 1번 데이터베이스 사용
+	 */
+	@Bean
+	@Qualifier("redisConnectionFactoryForRefreshToken")
+	LettuceConnectionFactory connectionFactory2() {
+		return createRedisConnectionFactory(1);
 	}
 
 	@Bean
-	public RedisTemplate<String, Search> searchRedisTemplate() {
+	public RedisTemplate<String, Search> searchRedisTemplate(
+		RedisConnectionFactory connectionFactory) {
 
 		RedisTemplate<String, Search> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(redisConnectionFactoryForSearch());
+		redisTemplate.setConnectionFactory(connectionFactory);
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 		redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Search.class));
@@ -48,5 +58,27 @@ public class RedisConfig {
 		return redisTemplate;
 	}
 
+	@Bean
+	public RedisTemplate<String, RefreshToken> refreshTokenRedisTemplate(
+		@Qualifier("redisConnectionFactoryForRefreshToken") RedisConnectionFactory redisConnectionFactory) {
+
+		RedisTemplate<String, RefreshToken> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(RefreshToken.class));
+		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(RefreshToken.class));
+
+		return redisTemplate;
+	}
+
+	public LettuceConnectionFactory createRedisConnectionFactory(final int idx) {
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+		config.setHostName(properties.getHost());
+		config.setPort(properties.getPort());
+		config.setPassword(properties.getPassword());
+		config.setDatabase(idx); // 0번 또는 1번 데이터베이스 지정
+		return new LettuceConnectionFactory(config);
+	}
 
 }
