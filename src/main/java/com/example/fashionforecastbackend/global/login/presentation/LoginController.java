1308139caee7043482bb66f0d93a12aca9b9f828
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-09-16 16:38:25 +0900

    refactor: 리프레시 토큰을 사용하는것으로 수정

--- src/main/java/com/example/fashionforecastbackend/global/login/presentation/LoginController.java
+++ src/main/java/com/example/fashionforecastbackend/global/login/presentation/LoginController.java
@@ -1,16 +1,14 @@
 package com.example.fashionforecastbackend.global.login.presentation;
 
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.fashionforecastbackend.global.login.dto.request.AccessTokenRequest;
 import com.example.fashionforecastbackend.global.login.dto.response.AccessTokenResponse;
 import com.example.fashionforecastbackend.global.login.service.LoginService;
 import com.example.fashionforecastbackend.global.response.ApiResponse;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,8 +19,8 @@ public class LoginController {
 	private final LoginService loginService;
 
 	@PostMapping("/token")
-	public ApiResponse<AccessTokenResponse> issueAccessToken(@Valid @RequestBody AccessTokenRequest request) {
-		return ApiResponse.created(loginService.issueAccessToken(request));
+	public ApiResponse<AccessTokenResponse> issueAccessToken(@CookieValue(value = "refresh_token") final String refreshTokenRequest) {
+		return ApiResponse.created(loginService.issueAccessToken(refreshTokenRequest));
 	}
 
 }
