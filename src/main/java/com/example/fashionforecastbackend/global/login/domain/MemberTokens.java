6731e098de772ac30a6b6cb2cad6c8a1d1ab008c
cherryiJuice <chldlwn711@gmail.com>
2024-09-20 15:52:45 +0900

    fix: 순서 변경

--- src/main/java/com/example/fashionforecastbackend/global/login/domain/MemberTokens.java
+++ src/main/java/com/example/fashionforecastbackend/global/login/domain/MemberTokens.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class MemberTokens {
-	@NotNull(message = "리프레시 토큰을 입력해주세요.")
-	private final String refreshToken;
 	@NotNull(message = "액세스 토큰을 입력해주세요.")
 	private final String accessToken;
+	@NotNull(message = "리프레시 토큰을 입력해주세요.")
+	private final String refreshToken;
 }
