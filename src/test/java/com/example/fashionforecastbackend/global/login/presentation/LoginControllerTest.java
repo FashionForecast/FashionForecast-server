49edf3abbe9b4a35040adc7c2b7050b47cfc0daa
cherryiJuice <chldlwn711@gmail.com>
2024-09-20 15:53:19 +0900

    test: 로그인 컨트롤러 테스트

--- src/test/java/com/example/fashionforecastbackend/global/login/presentation/LoginControllerTest.java
+++ src/test/java/com/example/fashionforecastbackend/global/login/presentation/LoginControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.fashionforecastbackend.global.login.presentation;
 
+import static com.example.fashionforecastbackend.global.restdocs.RestDocsConfiguration.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.cookies.CookieDocumentation.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -21,11 +23,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.example.fashionforecastbackend.global.ControllerTest;
+import com.example.fashionforecastbackend.global.login.domain.MemberTokens;
 import com.example.fashionforecastbackend.global.login.dto.response.AccessTokenResponse;
 import com.example.fashionforecastbackend.global.login.service.LoginService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebMvcTest(LoginController.class)
 @MockBean(JpaMetamodelMappingContext.class)
@@ -35,6 +39,8 @@ class LoginControllerTest extends ControllerTest {
 
 	private final static String ACCESS_TOKEN = "accessToken";
 	private final static String REFRESH_TOKEN = "refreshToken";
+	private final static String NEW_ACCESS_TOKEN = "newAccessToken";
+	private final static String NEW_REFRESH_TOKEN = "newRefreshToken";
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -76,6 +82,39 @@ class LoginControllerTest extends ControllerTest {
 					fieldWithPath("accessToken").type(JsonFieldType.STRING).description("액세스 토큰"))
 			));
 
+	}
+
+	@Test
+	@DisplayName("토큰 재발급")
+	void reissueTokensTest() throws Exception {
+		MemberTokens tokens = new MemberTokens(ACCESS_TOKEN, REFRESH_TOKEN);
+		MemberTokens newTokens = new MemberTokens(NEW_ACCESS_TOKEN, NEW_REFRESH_TOKEN);
+		given(loginService.renewTokens(any(MemberTokens.class), any(HttpServletResponse.class))).willReturn(newTokens);
+
+		mockMvc.perform(post("/api/v1/login/reissue")
+				.with(csrf())
+				.content(objectMapper.writeValueAsString(tokens))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.status").value(201))
+			.andExpect(jsonPath("$.message").value("CREATED"))
+			.andExpect(jsonPath("$.data.accessToken").value(NEW_ACCESS_TOKEN))
+			.andExpect(jsonPath("$.data.refreshToken").value(NEW_REFRESH_TOKEN))
+			.andDo(restDocs.document(
+				requestFields(
+					fieldWithPath("accessToken").description("액세스 토큰")
+						.attributes(field("format", "jwt")),
+					fieldWithPath("refreshToken").description("리프레시 토큰")
+						.attributes(field("format", "jwt"))
+				),
+				responseFields(
+					fieldWithPath("status").type(JsonFieldType.NUMBER).description("HttpStatus"),
+					fieldWithPath("message").type(JsonFieldType.STRING).description("상태 메세지")
+				).andWithPrefix("data.",
+					fieldWithPath("accessToken").type(JsonFieldType.STRING).description("액세스 토큰"),
+					fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰"))
+			));
 	}
 
 }
