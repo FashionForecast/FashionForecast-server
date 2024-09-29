28f6effeef1faab303008a7098b1d10aa85ae6bd
cherryiJuice <chldlwn711@gmail.com>
2024-09-30 02:27:19 +0900

    feat: 검증 추가

--- src/main/java/com/example/fashionforecastbackend/board/dto/request/BoardRequest.java
+++ src/main/java/com/example/fashionforecastbackend/board/dto/request/BoardRequest.java
@@ -1,6 +1,9 @@
 package com.example.fashionforecastbackend.board.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record BoardRequest(
+	@NotNull(message = "비어있을 수 없습니다.")
 	String text
 ) {
 }
