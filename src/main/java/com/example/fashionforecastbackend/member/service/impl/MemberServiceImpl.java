532e7a37efdb4682eddecc8a2ff893a98b978bbb
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-09 22:01:19 +0900

    feat: 멤버 옷차림 저장 서비스 레이어 구현

--- src/main/java/com/example/fashionforecastbackend/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/example/fashionforecastbackend/member/service/impl/MemberServiceImpl.java
@@ -7,8 +7,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.fashionforecastbackend.global.error.exception.MemberNotFoundException;
 import com.example.fashionforecastbackend.member.domain.Member;
+import com.example.fashionforecastbackend.member.domain.MemberOutfit;
+import com.example.fashionforecastbackend.member.domain.constant.BottomAttribute;
+import com.example.fashionforecastbackend.member.domain.constant.TopAttribute;
+import com.example.fashionforecastbackend.member.domain.repository.MemberOutfitRepository;
 import com.example.fashionforecastbackend.member.domain.repository.MemberRepository;
 import com.example.fashionforecastbackend.member.dto.request.MemberGenderRequest;
+import com.example.fashionforecastbackend.member.dto.request.MemberOutfitRequest;
 import com.example.fashionforecastbackend.member.dto.response.MemberInfoResponse;
 import com.example.fashionforecastbackend.member.service.MemberService;
 
@@ -20,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class MemberServiceImpl implements MemberService {
 
 	private final MemberRepository memberRepository;
+	private final MemberOutfitRepository memberOutfitRepository;
 
 	@Transactional
 	@Override
@@ -28,14 +34,36 @@ public class MemberServiceImpl implements MemberService {
 		return MemberInfoResponse.of(member);
 	}
 
+	@Transactional
 	@Override
 	public void saveGender(final MemberGenderRequest memberGenderRequest, final Long memberId) {
 		Member member = getById(memberId);
 		member.updateGender(memberGenderRequest.gender());
 	}
 
+	@Transactional
+	@Override
+	public void saveMemberOutfit(final MemberOutfitRequest memberOutfitRequest, final Long memberId) {
+		final Member member = getById(memberId);
+		final TopAttribute topAttribute = TopAttribute.of(memberOutfitRequest.topType(),
+			memberOutfitRequest.topColor());
+		final BottomAttribute bottomAttribute = BottomAttribute.of(memberOutfitRequest.bottomType(),
+			memberOutfitRequest.bottomColor());
+
+		final MemberOutfit memberOutfit = MemberOutfit.builder()
+			.topAttribute(topAttribute)
+			.bottomAttribute(bottomAttribute)
+			.build();
+
+		member.addMemberOutfit(memberOutfit);
+
+		memberOutfitRepository.save(memberOutfit);
+	}
+
 	private Member getById(final Long memberId) {
 		return memberRepository.findById(memberId)
 			.orElseThrow(() -> new MemberNotFoundException(NOT_FOUND_MEMBER));
 	}
+
+
 }
