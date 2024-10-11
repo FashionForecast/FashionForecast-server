c5c2e72fcc86b3366e471dba4196932c8cb8dfd0
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-11 16:45:14 +0900

    feat: 멤버id로 멤버 옷차림 조회 메서드 추가

--- src/main/java/com/example/fashionforecastbackend/member/domain/repository/MemberOutfitRepository.java
+++ src/main/java/com/example/fashionforecastbackend/member/domain/repository/MemberOutfitRepository.java
@@ -1,9 +1,16 @@
 package com.example.fashionforecastbackend.member.domain.repository;
 
+import java.util.LinkedList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.fashionforecastbackend.member.domain.MemberOutfit;
 
 public interface MemberOutfitRepository extends JpaRepository<MemberOutfit, Long> {
+
+	@Query("SELECT mo FROM MemberOutfit mo JOIN FETCH mo.tempStage ts WHERE mo.member.id = :memberId ORDER BY ts.level ASC")
+	LinkedList<MemberOutfit> findByMemberIdWithTempStage(@Param("memberId") final Long memberId);
 
 }
