d8219c164589927582bccc3193017de67243d2bd
cherryiJuice <chldlwn711@gmail.com>
2024-10-14 02:35:05 +0900

    fix: null 허용

--- src/main/java/com/example/fashionforecastbackend/member/dto/request/RegionRequest.java
+++ src/main/java/com/example/fashionforecastbackend/member/dto/request/RegionRequest.java
@@ -1,9 +1,6 @@
 package com.example.fashionforecastbackend.member.dto.request;
 
-import jakarta.validation.constraints.NotNull;
-
 public record RegionRequest(
-	@NotNull(message = "지역 이름을 입력해주세요.")
 	String region
 ) {
 }
