d9175e2b086a914b0a0530bd4e0e171c388cadbf
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-26 23:06:21 +0900

    feat: 최근 검색어 삭제 과정 추가

--- src/main/java/com/example/fashionforecastbackend/listener/DeleteEventListener.java
+++ src/main/java/com/example/fashionforecastbackend/listener/DeleteEventListener.java
@@ -5,9 +5,13 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import com.example.fashionforecastbackend.global.error.ErrorCode;
+import com.example.fashionforecastbackend.global.error.exception.MemberNotFoundException;
+import com.example.fashionforecastbackend.member.domain.Member;
 import com.example.fashionforecastbackend.member.domain.MemberDeleteEvent;
 import com.example.fashionforecastbackend.member.domain.repository.MemberOutfitRepository;
 import com.example.fashionforecastbackend.member.domain.repository.MemberRepository;
+import com.example.fashionforecastbackend.search.service.SearchService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +24,7 @@ public class DeleteEventListener {
 
 	private final MemberRepository memberRepository;
 	private final MemberOutfitRepository memberOutfitRepository;
+	private final SearchService searchService;
 
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
@@ -28,6 +33,15 @@ public class DeleteEventListener {
 		final Long memberId = event.memberId();
 		memberOutfitRepository.deleteAllByMemberId(memberId);
 		memberRepository.deleteById(memberId);
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	@TransactionalEventListener(fallbackExecution = true)
+	public void deleteAllSearch(final MemberDeleteEvent event) {
+		final Long memberId = event.memberId();
+		final Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> new MemberNotFoundException(ErrorCode.NOT_FOUND_MEMBER));
+		searchService.deleteAllSearch(member.getSocialId());
 	}
 
 }
