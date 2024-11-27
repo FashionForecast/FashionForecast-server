dc0bd64852dc5ccb6ef864f7c5148dd3f8f8ebb7
cherryiJuice <chldlwn711@gmail.com>
2024-11-27 18:08:04 +0900

    feat: 레디스 설정

---
+++ src/main/java/com/example/fashionforecastbackend/customOutfit/domain/GuestOutfit.java
@@ -0,0 +1,14 @@
+package com.example.fashionforecastbackend.customOutfit.domain;
+
+import lombok.Getter;
+
+@Getter
+public class GuestOutfit {
+
+	private int tempStageLevel;
+	private String topType;
+	private String topColor;
+	private String bottomType;
+	private String bottomColor;
+
+}
