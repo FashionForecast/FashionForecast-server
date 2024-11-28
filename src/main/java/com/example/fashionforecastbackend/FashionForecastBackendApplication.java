2da7656eebfda7331f9a47640c779f4b070e275a
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-11-28 18:10:13 +0900

    최근 검색어 uuid 검증 로직 추가 (#157)
    * feat: AOP 허용 애노이테션 추가
    
    * feat: 소셜 ID를 통해 멤버를 찾는 메서드 추가
    
    * feat: uuid검증을 위한 커스텀 애노테이션 구현
    
    * feat: uuid검증 aop 적용을 위한 체커 구현
    
    * refactor: uuid 유효성 검증 애노테이션 적용

--- src/main/java/com/example/fashionforecastbackend/FashionForecastBackendApplication.java
+++ src/main/java/com/example/fashionforecastbackend/FashionForecastBackendApplication.java
@@ -2,6 +2,7 @@ package com.example.fashionforecastbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableRetry
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class FashionForecastBackendApplication {
 
 	public static void main(String[] args) {
