54e9961d07c665962f934c68e314a91030325f0b
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-12-03 15:36:49 +0900

    feat: 특정 도시에 해당되는 지역 조회하는 메서드 추가

--- src/main/java/com/example/fashionforecastbackend/region/domain/repository/RegionRepository.java
+++ src/main/java/com/example/fashionforecastbackend/region/domain/repository/RegionRepository.java
@@ -1,5 +1,6 @@
 package com.example.fashionforecastbackend.region.domain.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,4 +18,7 @@ public interface RegionRepository extends JpaRepository<Region, Long>, RegionCus
 		@Param("neighborhood") String neighborhood);
 
 	Optional<Region> findByNxAndNy(double nx, double ny);
+
+	@Query("SELECT r FROM Region r WHERE r.address.city IN :cities")
+	List<Region> findByCities(@Param("cities") List<String> cities);
 }
